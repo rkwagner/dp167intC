dfbd4801fc44c58e056ac5277b70b727dd8b897b
Ryan Wagner <rkwagner@ucsd.edu>
2014-08-25 15:39:49 -0700

    adding insertion logic.

--- grade.c
+++ grade.c
@@ -11,37 +11,40 @@ struct stud{
   struct stud * next;
 };
 
+void insertOrder(struct stud * root, struct stud * current){
+  struct stud * next = root;
+  struct stud * previous;
+  while(next->avg < current->avg){
+    previous = next;
+    next = next->next;
+  }
+  previous->next = current;
+  current->next = next;
+}
+
 int main(void){
   FILE * in = fopen("input.dat", "r");
-  struct stud * root;
-  root = (struct stud *)malloc(sizeof(struct stud));
+  struct stud * root = (struct stud *)malloc(sizeof(struct stud));
   root->next=0;
   if(!feof(in)){
-    fscanf(in,"%s %*s %s %d %d %d %d %d", root->first, root->last,root->score,
-        &root->score[1],&root->score[2],&root->score[3],&root->score[4]);
+    fscanf(in,"%15[^,] %15[^0123456789] %d %d %d %d %d",root->first,
+        root->last,root->score,&root->score[1],&root->score[2],
+        &root->score[3],&root->score[4]);
     root->avg=(root->score[0]+root->score[1]+root->score[2]+
        root->score[3]+root->score[4])/5;
   }
   while(!feof(in)){
     struct stud * node = (struct stud *)malloc(sizeof(struct stud));
-    fscanf(in,"%s %*s %s %d %d %d %d %d", root->first, root->last,root->score,
-        &root->score[1],&root->score[2],&root->score[3],&root->score[4]);
-    root->avg=(root->score[0]+root->score[1]+root->score[2]+
-       root->score[3]+root->score[4])/5;
+    fscanf(in,"%15[^,] %15[^0123456789] %d %d %d %d %d",node->first,
+        node->last,node->score,&node->score[1],&node->score[2],
+        &node->score[3],&node->score[4]);
+    node->avg=(node->score[0]+node->score[1]+node->score[2]+
+       node->score[3]+node->score[4])/5;
     struct stud * current = root;
-    node->next=root;
-    while(node->avg >= node->next->avg){
-      current = node->next;
-      node->next=current->next;
-    }
-    if(node->next==0){
-      
-    }
-    else if(node->next->avg==root->avg){
-      root=node;
-    }
-    else{
-      current->next=node;
+    if(current != 0){
+      while(current->next != 0){
+        current=current->next;
+      }
     }
   }
   fclose(in);
