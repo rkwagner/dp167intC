2e2bb70306edd47d3bc82a09eede60fc568c8f8e
Ryan Wagner <rkwagner@ucsd.edu>
2014-08-25 21:02:16 -0700

    Project complete.

--- grade.c
+++ grade.c
@@ -2,55 +2,90 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct stud{
-  char first[30];
-  char last[30];
-  int score[5];
-  int avg;
+  char first[ 30 ], last[ 30 ];
+  int score[ 5 ];
+  double avg;
+  char * grade;
   struct stud * next;
 };
 
-void insertOrder(struct stud * root, struct stud * current){
-  struct stud * next = root;
-  struct stud * previous;
-  while(next->avg < current->avg){
-    previous = next;
-    next = next->next;
+struct stud * insert ( struct stud * root, struct stud * node ){
+  if ( root == 0 ){
+    root = node;
+    root->next = 0;
   }
-  previous->next = current;
-  current->next = next;
+  else if ( node->avg >= root->avg ){
+    node->next = root;
+    root = node;
+  }
+  else{
+    struct stud * curr = root;
+    while( node->avg < curr->next->avg ){
+      curr = curr->next;
+    }
+    while( node->avg == curr->next->avg && 
+        strcmp( node->last, curr->next->last ) > 0 ){
+      curr = curr->next;
+    }
+    node->next = curr->next;
+    curr->next = node;
+  }
+  return root;
 }
 
-int main(void){
-  FILE * in = fopen("input.dat", "r");
-  struct stud * root = (struct stud *)malloc(sizeof(struct stud));
-  root->next=0;
-  if(!feof(in)){
-    fscanf(in,"%15[^,] %15[^0123456789] %d %d %d %d %d",root->first,
-        root->last,root->score,&root->score[1],&root->score[2],
-        &root->score[3],&root->score[4]);
-    root->avg=(root->score[0]+root->score[1]+root->score[2]+
-       root->score[3]+root->score[4])/5;
+char * calc( struct stud * node ){
+   if( node->avg >= 98 )
+    return "A+";
+   if( node->avg >= 93 )
+    return "A";
+   if( node->avg >= 90 )
+    return "A-";
+   if( node->avg >= 87 )
+    return "B+";
+   if( node->avg >= 83 )
+    return "B";
+   if( node->avg >= 80 )
+    return "B-";
+   if( node->avg >= 77 )
+    return "C+";
+   if( node->avg >= 73 )
+    return "C";
+   if( node->avg >= 70 )
+    return "C-";
+   if( node->avg >= 67 )
+    return "D+";
+   if( node->avg >= 63 )
+    return "D";
+   if( node->avg >= 60 )
+    return "D-";
+   return "F"; 
+}
+
+int main( void ){
+  FILE * in = fopen( "input.dat", "r" );
+  struct stud * root = ( struct stud * )malloc( sizeof( struct stud ) );
+  char list[ 150 ];
+  while( fgets( list, 150, in ) ){
+    struct stud * node = ( struct stud * )malloc( sizeof( struct stud ) );
+    sscanf( list,
+        "%15[^,]%*[^ABCDEFGHIJKLMNOPQRSTUVWXYZ]%15[^0123456789]%d%d%d%d%d",
+        node->first, node->last, node->score, &node->score[ 1 ],
+        &node->score[ 2 ], &node->score[ 3 ], &node->score[ 4 ]);
+    node->avg = ( node->score[ 0 ] + node->score[ 1 ] + node->score[ 2 ] +
+        node->score[ 3 ] + node->score[ 4 ] ) / 5;
+    node->grade = calc( node );
+    root = insert( root, node );
   }
-  while(!feof(in)){
-    struct stud * node = (struct stud *)malloc(sizeof(struct stud));
-    fscanf(in,"%15[^,] %15[^0123456789] %d %d %d %d %d",node->first,
-        node->last,node->score,&node->score[1],&node->score[2],
-        &node->score[3],&node->score[4]);
-    node->avg=(node->score[0]+node->score[1]+node->score[2]+
-       node->score[3]+node->score[4])/5;
-    struct stud * current = root;
-    if(current != 0){
-      while(current->next != 0){
-        current=current->next;
-      }
-    }
+  ( void )printf( "%-15s%-15s%-6s%5s%5s%5s%5s%5s%5s\n", "First",
+      "Last", "Grade", "Pct.", "T1", "T2", "T3", "T4", "T5" );
+  while( root->next != 0 ){
+    ( void )printf( "%-15s%-15s%-6s%5.1lf%5d%5d%5d%5d%5d\n", root->first,
+        root->last, root->grade, root->avg, root->score[ 0 ],
+        root->score[ 1 ],root->score[ 2 ],root->score[ 3 ],root->score[ 4 ] );
+    root = root->next;
   }
-  fclose(in);
-  struct stud * current=root;
-  while(current->next!=0){
-    (void)printf("%s\n", current->first);
-    current=current->next;
-  }
+  fclose( in );
 }
